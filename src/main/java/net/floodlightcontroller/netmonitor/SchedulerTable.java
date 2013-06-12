dd49cff06372eefc5a90391ee96da6f44a9034d1
shihab <sr2chowd@ubuntu.ubuntu-domain>
2013-06-12 02:54:00 -0400

    new file:   src/main/java/net/floodlightcontroller/netmonitor/SchedulerTable.java

---
+++ src/main/java/net/floodlightcontroller/netmonitor/SchedulerTable.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.floodlightcontroller.netmonitor;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ScheduledExecutorService;
+import net.floodlightcontroller.core.util.SingletonTask;
+
+/**
+ *
+ * @author sr2chowd
+ */
+public class SchedulerTable {
+    private ConcurrentMap < Integer, ArrayList <FlowEntry> > scheduleTable;
+    private ConcurrentMap < Integer, SingletonTask > actionSet;
+    
+    public SchedulerTable()
+    {
+        scheduleTable = new ConcurrentHashMap<Integer, ArrayList <FlowEntry> >();
+        actionSet = new ConcurrentHashMap<Integer, SingletonTask>();
+    }
+    
+    public synchronized void addFlowEntry(Integer timeout, FlowEntry entry)
+    {
+        if(scheduleTable.get(timeout) == null)
+        {
+            scheduleTable.put(timeout, new ArrayList<FlowEntry>());
+        }
+        scheduleTable.get(timeout).add(entry);
+    }
+    
+    public synchronized void addAction(Integer timeout, SingletonTask action)
+    {
+        actionSet.put(timeout, action);
+    }
+    
+    public SingletonTask getAction(Integer timeout)
+    {
+        return actionSet.get(timeout);
+    }
+    
+    public synchronized void updateTimeout(Integer oldTimeout, Integer newTimeout, FlowEntry entry)
+    {
+        scheduleTable.get(oldTimeout).remove(entry);
+        this.addFlowEntry(newTimeout, entry);
+    }
+    
+    public ArrayList <FlowEntry> getAllEntries(Integer timeout)
+    {
+        return scheduleTable.get(timeout);
+    }
+}
